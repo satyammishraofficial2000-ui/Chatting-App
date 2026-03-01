33e975ded66906374f78e3b0c3af7c01c317f9e1
satyam <your-email@example.comsatyammishraofficial2000@gmail.com>
2026-03-02 00:07:37 +0530

    adding config.env file

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
+const dotenv = reuire('dotenv');
+dotenv.config({path : './config.env'});
 let app = require('./app')
 
-const port =3000;
+const port =process.env.PORT_NUMBER || 3000;
 
 app.listen(port , () => {
     console.log("listening to the request" + port);
