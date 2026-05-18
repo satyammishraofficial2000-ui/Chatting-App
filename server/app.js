a2bb673ecf2dc8e759dfe510cc008d8f37744d44
satyam mishra <satyammishraofficial2000gmail.com>
2026-05-18 15:51:43 +0530

    fronted url

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 app.use(cors({
-   origin: "https://chatting-pmtsid2ay-satyam-mishras-projects-a4cffc01.vercel.app",
+   origin: "https://chatting-app-git-main-satyam-mishras-projects-a4cffc01.vercel.app",
    credentials: true
 }))
 
@@ -18,7 +18,7 @@ app.use(express.json({
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
      cors: {
-          origin: 'https://chatting-pmtsid2ay-satyam-mishras-projects-a4cffc01.vercel.app',
+          origin: 'https://chatting-app-git-main-satyam-mishras-projects-a4cffc01.vercel.app',
           methods: ['GET', 'POST']
      }
 });
